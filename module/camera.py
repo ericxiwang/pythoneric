05fad849fd42117ccae79f6b03bd68809f5a77a3
xi.wang <xi.wang@xsight.ai>
2018-11-21 18:34:42 -0800

    python3 opencv

--- module/camera.py
+++ module/camera.py
@@ -1,12 +1,12 @@
 import cv2
-
+import time
 
 class VideoCamera(object):
     def __init__(self):
         # Using OpenCV to capture from device 0. If you have trouble capturing
         # from a webcam, comment the line below out and use a video file
         # instead.
-        #self.video = cv2.VideoCapture('rtsp://admin:xsight@123@10.16.10.222:554/Streaming/Channels/001')
+        self.video = cv2.VideoCapture('rtsp://admin:xsight@123@10.16.10.222:554/Streaming/Channels/001')
         self.img_path = "static/images/faces/eric.jpg"
         self.cascPath_1 = "/usr/local/lib/python3.7/site-packages/cv2/data/haarcascade_frontalface_default.xml"
         self.cascPath_2 = "/usr/local/lib/python3.7/site-packages/cv2/data/haarcascade_eye.xml"
@@ -14,17 +14,17 @@ class VideoCamera(object):
         # as the main.py.
         # self.video = cv2.VideoCapture('video.mp4')
 
-    #def __del__(self):
-     #   self.video.release()
+    def __del__(self):
+        self.video.release()
 
     def get_frame(self):
         faceCascade = cv2.CascadeClassifier(self.cascPath_1)
 
-        #success, image = self.video.read()
+        success, image = self.video.read()
         # We are using Motion JPEG, but OpenCV defaults to capture raw images,
         # so we must encode it into JPEG in order to correctly display the
         # video stream.
-        image = cv2.imread(self.img_path)
+        '''image = cv2.imread(self.img_path)
 
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
@@ -32,7 +32,7 @@ class VideoCamera(object):
         faces = faceCascade.detectMultiScale(gray, scaleFactor=1.2, minNeighbors=5, minSize=(30, 30))
         print(faces)
         for (x, y, w, h) in faces:
-            cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)'''
 
         ret, jpeg = cv2.imencode('.jpg', image)
 
@@ -44,30 +44,7 @@ class VideoCamera(object):
         return jpeg.tobytes()
 
 
-    def get_image(self):
-        faceCascade = cv2.CascadeClassifier(self.cascPath_1)
-
-        # Read the image
-        image = cv2.imread('trump.jpg')
-        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-        # Detect faces in the image
-        faces = faceCascade.detectMultiScale(gray, scaleFactor=1.2, minNeighbors=5, minSize=(30, 30))
-        for (x, y, w, h) in faces:
-            cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
-
-        return image
-
 
 
 if __name__ == '__main__':
-    vcap=VideoCamera("rtsp://admin:xsight@123@10.16.10.222:554/Streaming/Channels/001")
-    '''while (1):
-        ret, frame = vcap.video.read()
-        cv2.imshow('video', frame)
-        cv2.waitKey(1)'''
-
-    a=vcap.get_image()
-    cv2.imshow('img', a)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    print("AA")
--- module/views.py
+++ module/views.py
@@ -6,26 +6,23 @@ camera_link="rtsp://admin:xsight@123@10.16.10.222:554/Streaming/Channels/001"
 from camera import VideoCamera
 app = Flask(__name__)
 
-
-
 @app.route('/')
 def index():
     return render_template('index.html')
 
 def gen(camera):
-    frame = camera.get_image()
-    yield (b'--frame\r\n'
-           b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
-    '''while True:
+
+
+    while True:
         frame = camera.get_frame()
         yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')'''
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
 @app.route('/video_feed')
-def video_feed(link=camera_link):
+def video_feed():
 
-    return Response(gen(VideoCamera(link)),
+    return Response(gen(VideoCamera()),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
